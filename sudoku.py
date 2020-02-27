201ac20745c201ef0da86e8943c53459b10638ed
Jacek <Jacek6000@gmail.com>
2020-02-27 13:12:56 +0100

    fixing performance issue

--- sudoku.py
+++ sudoku.py
@@ -3,15 +3,16 @@ import numpy as np
 
 def solve(board, x=0, y=0):
     if x > 8:
-        y = y + 1
+        y += 1
         x = 0
         if y == 9:
             return True
 
     if board[y, x] == 0:
+        invalid = find_invalid(board, x, y)
         for guess in range(1, 10):
             board[y, x] = guess
-            if valid(board, x, y):
+            if guess not in invalid:
                 if solve(board, x+1, y):
                     return True
             board[y, x] = 0
@@ -21,40 +22,44 @@ def solve(board, x=0, y=0):
             return True
 
 
-def valid(board, x, y):
+def find_invalid(board, x, y):
+    seen = set()
     # row
-    if not_unique(board[y, :]):
-        return False
-
+    seen = find_unique(board[y, :], seen)
     # column
-    if not_unique(board[:, x]):
-        return False
-
+    seen = find_unique(board[:, x], seen)
     # subbox
     subx = x//3
     suby = y//3
-    if not_unique(board[suby*3: (suby+1)*3, subx*3: (subx+1)*3]):
-        return False
-
-    return True
+    find_unique(board[suby*3: (suby+1)*3, subx*3: (subx+1)*3].reshape(9), seen)
+    return seen
 
 
-def not_unique(subboard):
-    if np.unique(subboard).sum() != subboard.sum():
-        return True
-    else:
-        return False
+def find_unique(subboard, seen):
+    for i in range(0, 9):
+        seen.add(subboard[i])
+    return seen
 
-if __name__ == '__main__':
+
+def test():
     board = np.fromfile("sudoku_board.txt", dtype=np.int32,
                         sep=" ").reshape((9, 9))
-    print(solve(board))
+    solve(board)
+    print(board)
     board = np.fromfile("sudoku_board2.txt", dtype=np.int32,
                         sep=" ").reshape((9, 9))
-    print(solve(board))
+    solve(board)
+    print(board)
     board = np.fromfile("sudoku_board3.txt", dtype=np.int32,
                         sep=" ").reshape((9, 9))
-    print(solve(board))
+    solve(board)
+    print(board)
     board = np.fromfile("sudoku_solved.txt", dtype=np.int32,
                         sep=" ").reshape((9, 9))
-    print(solve(board))
+    solve(board)
+    print(board)
+
+
+if __name__ == '__main__':
+    import cProfile
+    cProfile.run('test()')
